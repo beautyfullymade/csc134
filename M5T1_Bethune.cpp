784778b940cf2bfb8b470f53f7cf617a934d4cf8
Toni Bethune <bethunet7773@student.faytechcc.edu>
2025-11-11 00:56:53 +0000

    finished

---
+++ M5T1_Bethune.cpp
@@ -0,0 +1,34 @@
+// CSC 134
+// M5T1 - Functions (Final Version)
+// Toni Bethune
+// 11/10/2025
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Function declarations (prototypes)
+string formatAnswer(int answer);
+void printAnswer(string msg);
+
+int main() {
+    int answer = 5;
+    string message;
+    message = formatAnswer(answer);
+    printAnswer(message);
+    return 0;
+}
+
+// Function definitions
+string formatAnswer(int answer) {
+    // Make a nice looking string containing the answer
+    string answerMessage;
+    answerMessage = "The answer is ";
+    answerMessage += to_string(answer);
+    return answerMessage;
+}
+
+void printAnswer(string msg) {
+    // display our message
+    cout << msg << endl;
+}
